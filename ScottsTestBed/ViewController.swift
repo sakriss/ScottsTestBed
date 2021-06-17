32e475d5890322d73cc32f8a5e33c3eef619ff5d
Scott <scottkriss@gmail.com>
2021-06-16 20:42:32 -0700

    Initial Commit

---
+++ ScottsTestBed/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScottsTestBed
+//
+//  Created by Scott Kriss on 6/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
